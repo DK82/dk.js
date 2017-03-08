5416fee319ac3ea8b87a86ba333dbf8d302e9878
David <david.kearney@BESTINVEST.CO.UK>
2017-03-08 16:26:37 +0000

    test

--- dk.js
+++ dk.js
@@ -135,7 +135,7 @@ var dk =
             }
         }
     },
-    test:{
+    test: function(){
             var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
             var data = { q : "Portland,OR", units : "metric"  };
             function showWeather(response) { 
