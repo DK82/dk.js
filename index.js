1e05f3809163e178e0fe8a6d04ace3d3b48f2a13
David <dkearney82@gmail.com>
2022-11-19 13:08:12 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-module.exports = Object.assign({}, {
+// module.exports = Object.assign({}, {
+
+//     array: require('./src/array'),
+//     highchart: require('./src/highchart'),
+//     obj: require('./src/obj'),
+//     request: require('./src/request')
+
+// });
+let dk = {
 
     array: require('./src/array'),
     highchart: require('./src/highchart'),
     obj: require('./src/obj'),
     request: require('./src/request')
 
-});
+}
+
+module.exports = dk
